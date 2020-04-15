0d19d254bc8d268939d8af501876c14417f0c11b
Stuthedian <talybov.asif@yandex.ru>
2020-04-15 10:55:26 +0700

    First report

--- Lab3/Lab3/Form1.Designer.cs
+++ Lab3/Lab3/Form1.Designer.cs
@@ -28,57 +28,157 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(800, 387);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.dataGridView2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 361);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Controls.Add(this.dataGridView3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 361);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(192, 74);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(792, 361);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(22, 47);
+            this.dataGridView1.Location = new System.Drawing.Point(8, 6);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
-            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.TabIndex = 2;
             // 
             // dataGridView2
             // 
             this.dataGridView2.AllowUserToAddRows = false;
             this.dataGridView2.AllowUserToDeleteRows = false;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(268, 47);
+            this.dataGridView2.Location = new System.Drawing.Point(254, 6);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
             this.dataGridView2.RowHeadersVisible = false;
             this.dataGridView2.Size = new System.Drawing.Size(448, 150);
-            this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AllowUserToAddRows = false;
+            this.dataGridView3.AllowUserToDeleteRows = false;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(199, 19);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.RowHeadersVisible = false;
+            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView3.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(27, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(800, 387);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
 
--- Lab3/Lab3/Form1.cs
+++ Lab3/Lab3/Form1.cs
@@ -24,20 +24,17 @@ namespace Lab3
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            /*
+             Создание xml'ки!!
+             */
             doc = XElement.Load(@"../../Zaselenie.xml");
             root = new Root();
             root.komnatas = new List<Komnata>();
             root.clients = new List<Client>();
 
             List<Zaselenie> zaselenies = new List<Zaselenie>();
-            //Komnata temp_komnata = new Komnata();
-            //temp_komnata.zaselenies = new List<Zaselenie>();
-            //Zaselenie temp_zaselenie = new Zaselenie();
             foreach (var komnata in doc.Elements("комната"))
             {
-                //temp_komnata.nomer = Convert.ToInt32(komnata.Attribute("номер").Value);
-                //temp_komnata.vmestimost = Convert.ToInt32(komnata.Element("вместимость").Value);
                 zaselenies.Clear();
                 foreach (var zaselenie in komnata.Elements("заселение"))
                 {
@@ -57,7 +54,6 @@ namespace Lab3
                 });
             }
 
-            //Client temp_client = new Client();
             foreach (var client in doc.Element("клиенты").Elements("клиент"))
             {
                 root.clients.Add(new Client()
@@ -69,10 +65,22 @@ namespace Lab3
             }
 
             dataGridView1.DataSource = root.komnatas.Select(n => new { номер = n.nomer, вместимость = n.vmestimost }).ToArray();
-            //    dataGridView1.DataSource = doc.Elements("комната").Attributes("номер")
-            //    .Select(n => new { номер = n.Value, вместимость = n.Parent.Element("вместимость").Value } ).ToArray();
             dataGridView1.SelectionChanged += DataGridView1_SelectionChanged;
             DataGridView1_SelectionChanged(sender, e);
+
+            comboBox1.DataSource = root.komnatas.Select(n => n.nomer).ToArray();
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            ComboBox1_SelectedIndexChanged(sender, e);
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dataGridView3.DataSource = root.komnatas.Where(n => n.nomer.ToString() == comboBox1.SelectedItem.ToString())
+                .Select(n => new
+                {
+                    кол_во = n.zaselenies.Count,
+                    кол_во_разных = n.zaselenies.Select(cl => cl.cod).Distinct().Count()
+                }).ToArray();
         }
 
         private void DataGridView1_SelectionChanged(object sender, EventArgs e)
@@ -87,15 +95,6 @@ namespace Lab3
                     дата_заселения = elem.date_zas,
                     дата_освобождения = elem.date_osv
                 }).ToArray();
-            //dataGridView2.DataSource = doc.Elements("комната")
-            //    .Where(elem => elem.Attribute("номер").Value == dataGridView1.SelectedRows[0].Cells[0].Value.ToString())
-            //    .Single().Elements("заселение")
-            //    .Select(elem => new
-            //    {
-            //        Цена = elem.Element("Цена").Value,
-            //        дата_заселения = elem.Element("дата_заселения").Value,
-            //        дата_освобождения = elem.Element("дата_освобождения").Value
-            //    }).ToArray();
         }
     }
 }
