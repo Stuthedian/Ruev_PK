a65a829814e34e9172df328a21fcc29a2693064b
Stuthedian <talybov.asif@yandex.ru>
2020-04-15 10:43:03 +0700

    Datagridview output done

--- Lab3/Lab3/Form1.cs
+++ Lab3/Lab3/Form1.cs
@@ -30,43 +30,63 @@ namespace Lab3
             root.komnatas = new List<Komnata>();
             root.clients = new List<Client>();
 
-            Komnata temp_komnata = new Komnata();
-            temp_komnata.zaselenies = new List<Zaselenie>();
-            Zaselenie temp_zaselenie = new Zaselenie();
+            List<Zaselenie> zaselenies = new List<Zaselenie>();
+            //Komnata temp_komnata = new Komnata();
+            //temp_komnata.zaselenies = new List<Zaselenie>();
+            //Zaselenie temp_zaselenie = new Zaselenie();
             foreach (var komnata in doc.Elements("комната"))
             {
-                temp_komnata.nomer = Convert.ToInt32(komnata.Attribute("номер").Value);
-                temp_komnata.vmestimost = Convert.ToInt32(komnata.Element("вместимость").Value);
-                temp_komnata.zaselenies.Clear();
+                //temp_komnata.nomer = Convert.ToInt32(komnata.Attribute("номер").Value);
+                //temp_komnata.vmestimost = Convert.ToInt32(komnata.Element("вместимость").Value);
+                zaselenies.Clear();
                 foreach (var zaselenie in komnata.Elements("заселение"))
                 {
-                    temp_zaselenie.cod = Convert.ToInt32(zaselenie.Element("код_кл").Value);
-                    temp_zaselenie.cena = Convert.ToDouble(zaselenie.Element("Цена").Value, CultureInfo.InvariantCulture);
-                    temp_zaselenie.date_zas = Convert.ToDateTime(zaselenie.Element("дата_заселения").Value);
-                    temp_zaselenie.date_osv = Convert.ToDateTime(zaselenie.Element("дата_освобождения").Value);
+                    zaselenies.Add(new Zaselenie()
+                    {
+                        cod = Convert.ToInt32(zaselenie.Element("код_кл").Value),
+                        cena = Convert.ToDouble(zaselenie.Element("Цена").Value, CultureInfo.InvariantCulture),
+                        date_zas = Convert.ToDateTime(zaselenie.Element("дата_заселения").Value),
+                        date_osv = Convert.ToDateTime(zaselenie.Element("дата_освобождения").Value)
+                    });
                 }
-                temp_komnata.zaselenies.Add(temp_zaselenie);
-                root.komnatas.Add(temp_komnata);
+                root.komnatas.Add(new Komnata()
+                {
+                    nomer = Convert.ToInt32(komnata.Attribute("номер").Value),
+                    vmestimost = Convert.ToInt32(komnata.Element("вместимость").Value),
+                    zaselenies = zaselenies.ToList()
+                });
             }
 
-            Client temp_client = new Client();
+            //Client temp_client = new Client();
             foreach (var client in doc.Element("клиенты").Elements("клиент"))
             {
-                temp_client.cod = Convert.ToInt32(client.Element("код_кл").Value);
-                temp_client.fio = client.Element("фио").Value;
-                temp_client.pol = client.Element("пол").Value[0];
-
-                root.clients.Add(temp_client);
+                root.clients.Add(new Client()
+                {
+                    cod = Convert.ToInt32(client.Element("код_кл").Value),
+                    fio = client.Element("фио").Value,
+                    pol = client.Element("пол").Value[0]
+                });
             }
 
+            dataGridView1.DataSource = root.komnatas.Select(n => new { номер = n.nomer, вместимость = n.vmestimost }).ToArray();
             //    dataGridView1.DataSource = doc.Elements("комната").Attributes("номер")
             //    .Select(n => new { номер = n.Value, вместимость = n.Parent.Element("вместимость").Value } ).ToArray();
-            //dataGridView1.SelectionChanged += DataGridView1_SelectionChanged;
-            //DataGridView1_SelectionChanged(sender, e);
+            dataGridView1.SelectionChanged += DataGridView1_SelectionChanged;
+            DataGridView1_SelectionChanged(sender, e);
         }
 
         private void DataGridView1_SelectionChanged(object sender, EventArgs e)
         {
+            dataGridView2.DataSource = root.komnatas
+                .Where(elem => elem.nomer.ToString() == dataGridView1.SelectedRows[0].Cells[0].Value.ToString())
+                .Single().zaselenies
+                .Select(elem => new
+                {
+                    клиент = root.clients.Where(n => n.cod == elem.cod).Single().fio,
+                    Цена = elem.cena,
+                    дата_заселения = elem.date_zas,
+                    дата_освобождения = elem.date_osv
+                }).ToArray();
             //dataGridView2.DataSource = doc.Elements("комната")
             //    .Where(elem => elem.Attribute("номер").Value == dataGridView1.SelectedRows[0].Cells[0].Value.ToString())
             //    .Single().Elements("заселение")
