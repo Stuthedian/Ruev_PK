d06376ffb25a4378745ecebc15082bdc1a54b25d
Stuthedian <talybov.asif@yandex.ru>
2020-04-15 09:04:42 +0700

    Lab3

---
+++ Lab3/Lab3/Root.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Root
+    {
+        List<Komnata> komnatas;
+        List<Client> clients;
+    }
+}
