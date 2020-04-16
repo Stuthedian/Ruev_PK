ef27b7c553f52dd0146927928d612c8b9378cd9f
Stuthedian <talybov.asif@yandex.ru>
2020-04-16 16:09:59 +0700

    Functions to generate and load xml

--- Lab3/Lab3/Form1.cs
+++ Lab3/Lab3/Form1.cs
@@ -15,127 +15,19 @@ namespace Lab3
 {
     public partial class Form1 : Form
     {
-        XElement doc;
         Root root;
         public Form1()
         {
             InitializeComponent();
-            Root temp_root = new Root()
-            {
-                komnatas = new List<Komnata>()
-                {
-                    new Komnata(){ nomer = 1, vmestimost = 2,
-                        zaselenies = new List<Zaselenie>()
-                        {
-                            new Zaselenie() { cod = 1, cena = 1300,
-                                date_zas = Convert.ToDateTime("2018.05.04"),
-                                date_osv = Convert.ToDateTime("2018.06.04") }
-                        } },
-                    new Komnata(){ nomer = 11, vmestimost = 1,
-                        zaselenies = new List<Zaselenie>()
-                        {
-                            new Zaselenie() { cod = 1, cena = 1300,
-                                date_zas = Convert.ToDateTime("2018.05.04"),
-                                date_osv = Convert.ToDateTime("2018.06.04") },
-                            new Zaselenie() { cod = 2, cena = 1300.50,
-                                date_zas = Convert.ToDateTime("2018.08.24"),
-                                date_osv = Convert.ToDateTime("2018.06.04") }
-                        } },
-                    new Komnata(){ nomer = 12, vmestimost = 3,
-                        zaselenies = new List<Zaselenie>()
-                        {
-                            new Zaselenie() { cod = 1, cena = 1300,
-                                date_zas = Convert.ToDateTime("2018.06.02"),
-                                date_osv = Convert.ToDateTime("2018.09.04") },
-                            new Zaselenie() { cod = 2, cena = 1360.50,
-                                date_zas = Convert.ToDateTime("2018.09.24"),
-                                date_osv = Convert.ToDateTime("2018.12.04") },
-                            new Zaselenie() { cod = 2, cena = 1380.80,
-                                date_zas = Convert.ToDateTime("2018.02.24"),
-                                date_osv = Convert.ToDateTime("2018.06.04") }
-                        } }
-                },
-                clients = new List<Client>()
-                {
-                    new Client(){ cod = 1, fio = "Иванов А.Н.", pol = 'м'},
-                    new Client(){ cod = 2, fio = "Иванова Н.А.", pol = 'ж'},
-                    new Client(){ cod = 3, fio = "Петров В.А.", pol = 'м'}
-                }
-            };
-
-            doc = new XElement("Root", 
-                temp_root.komnatas.Select(k =>
-                new XElement("комната", new XAttribute("номер", k.nomer),
-                    new XElement("вместимость", k.vmestimost),
-                    k.zaselenies.Select(z => new XElement("заселение", 
-                        new XElement("код_кл", z.cod),
-                        new XElement("Цена", z.cena), 
-                        new XElement("дата_заселения", z.date_zas.ToShortDateString()),
-                        new XElement("дата_освобождения", z.date_osv.ToShortDateString()))))),
-                new XElement("клиенты", 
-                    temp_root.clients
-                    .Select(cl =>
-                new XElement("клиент", new XElement("код_кл", cl.cod), new XElement("фио", cl.fio),
-                    new XElement("пол", cl.pol)))
-                )
-                );
-            doc.Save("../../Zaselenie.xml");
+            root = new Root();
+            root.komnatas = new List<Komnata>();
+            root.clients = new List<Client>();
+            generate_xml();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            doc = XElement.Load("../../Zaselenie.xml");
-            root = new Root();
-            root.komnatas = new List<Komnata>();
-            root.clients = new List<Client>();
-
-            List<Zaselenie> zaselenies = new List<Zaselenie>();
-            foreach (var komnata in doc.Elements("комната"))
-            {
-                zaselenies.Clear();
-                foreach (var zaselenie in komnata.Elements("заселение"))
-                {
-                    zaselenies.Add(new Zaselenie()
-                    {
-                        cod = Convert.ToInt32(zaselenie.Element("код_кл").Value),
-                        cena = Convert.ToDouble(zaselenie.Element("Цена").Value, CultureInfo.InvariantCulture),
-                        date_zas = Convert.ToDateTime(zaselenie.Element("дата_заселения").Value),
-                        date_osv = Convert.ToDateTime(zaselenie.Element("дата_освобождения").Value)
-                    });
-                }
-                root.komnatas.Add(new Komnata()
-                {
-                    nomer = Convert.ToInt32(komnata.Attribute("номер").Value),
-                    vmestimost = Convert.ToInt32(komnata.Element("вместимость").Value),
-                    zaselenies = zaselenies.ToList()
-                });
-            }
-
-            foreach (var client in doc.Element("клиенты").Elements("клиент"))
-            {
-                root.clients.Add(new Client()
-                {
-                    cod = Convert.ToInt32(client.Element("код_кл").Value),
-                    fio = client.Element("фио").Value,
-                    pol = client.Element("пол").Value[0]
-                });
-            }
-
-            dataGridView1.DataSource = root.komnatas.Select(n => new { номер = n.nomer, вместимость = n.vmestimost }).ToArray();
-            dataGridView1.SelectionChanged += DataGridView1_SelectionChanged;
-            DataGridView1_SelectionChanged(sender, e);
-
-            comboBox1.DataSource = root.komnatas.Select(n => n.nomer).ToArray();
-            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
-            ComboBox1_SelectedIndexChanged(sender, e);
-
-            comboBox2.DataSource = root.komnatas.Select(n => n.nomer).ToArray();
-            comboBox2.SelectedIndexChanged += ComboBox2_SelectedIndexChanged;
-            ComboBox2_SelectedIndexChanged(sender, e);
-
-            comboBox3.DataSource = root.clients.Select(n => n.fio).ToArray();
-            comboBox3.SelectedIndexChanged += ComboBox3_SelectedIndexChanged;
-            ComboBox3_SelectedIndexChanged(sender, e);
+            load_xml("../../Zaselenie.xml");
         }
 
         private void ComboBox3_SelectedIndexChanged(object sender, EventArgs e)
@@ -191,14 +83,143 @@ namespace Lab3
                 }).ToArray();
         }
 
+        private void generate_xml()
+        {
+            XElement doc;
+            Root temp_root = new Root()
+            {
+                komnatas = new List<Komnata>()
+                {
+                    new Komnata(){ nomer = 1, vmestimost = 2,
+                        zaselenies = new List<Zaselenie>()
+                        {
+                            new Zaselenie() { cod = 1, cena = 1300,
+                                date_zas = Convert.ToDateTime("2018.05.04"),
+                                date_osv = Convert.ToDateTime("2018.06.04") }
+                        } },
+                    new Komnata(){ nomer = 11, vmestimost = 1,
+                        zaselenies = new List<Zaselenie>()
+                        {
+                            new Zaselenie() { cod = 1, cena = 1300,
+                                date_zas = Convert.ToDateTime("2018.05.04"),
+                                date_osv = Convert.ToDateTime("2018.06.04") },
+                            new Zaselenie() { cod = 2, cena = 1300.50,
+                                date_zas = Convert.ToDateTime("2018.08.24"),
+                                date_osv = Convert.ToDateTime("2018.06.04") }
+                        } },
+                    new Komnata(){ nomer = 12, vmestimost = 3,
+                        zaselenies = new List<Zaselenie>()
+                        {
+                            new Zaselenie() { cod = 1, cena = 1300,
+                                date_zas = Convert.ToDateTime("2018.06.02"),
+                                date_osv = Convert.ToDateTime("2018.09.04") },
+                            new Zaselenie() { cod = 2, cena = 1360.50,
+                                date_zas = Convert.ToDateTime("2018.09.24"),
+                                date_osv = Convert.ToDateTime("2018.12.04") },
+                            new Zaselenie() { cod = 2, cena = 1380.80,
+                                date_zas = Convert.ToDateTime("2018.02.24"),
+                                date_osv = Convert.ToDateTime("2018.06.04") }
+                        } }
+                },
+                clients = new List<Client>()
+                {
+                    new Client(){ cod = 1, fio = "Иванов А.Н.", pol = 'м'},
+                    new Client(){ cod = 2, fio = "Иванова Н.А.", pol = 'ж'},
+                    new Client(){ cod = 3, fio = "Петров В.А.", pol = 'м'}
+                }
+            };
+
+            doc = new XElement("Root",
+                temp_root.komnatas.Select(k =>
+                new XElement("комната", new XAttribute("номер", k.nomer),
+                    new XElement("вместимость", k.vmestimost),
+                    k.zaselenies.Select(z => new XElement("заселение",
+                        new XElement("код_кл", z.cod),
+                        new XElement("Цена", z.cena),
+                        new XElement("дата_заселения", z.date_zas.ToShortDateString()),
+                        new XElement("дата_освобождения", z.date_osv.ToShortDateString()))))),
+                new XElement("клиенты",
+                    temp_root.clients
+                    .Select(cl =>
+                new XElement("клиент", new XElement("код_кл", cl.cod), new XElement("фио", cl.fio),
+                    new XElement("пол", cl.pol)))
+                )
+                );
+            doc.Save("../../Zaselenie.xml");
+        }
+
+        private void load_xml(string file)
+        {
+            root.komnatas.Clear();
+            root.clients.Clear();
+
+            XElement doc = XElement.Load(file);
+
+            List<Zaselenie> zaselenies = new List<Zaselenie>();
+            foreach (var komnata in doc.Elements("комната"))
+            {
+                zaselenies.Clear();
+                foreach (var zaselenie in komnata.Elements("заселение"))
+                {
+                    zaselenies.Add(new Zaselenie()
+                    {
+                        cod = Convert.ToInt32(zaselenie.Element("код_кл").Value),
+                        cena = Convert.ToDouble(zaselenie.Element("Цена").Value, CultureInfo.InvariantCulture),
+                        date_zas = Convert.ToDateTime(zaselenie.Element("дата_заселения").Value),
+                        date_osv = Convert.ToDateTime(zaselenie.Element("дата_освобождения").Value)
+                    });
+                }
+                root.komnatas.Add(new Komnata()
+                {
+                    nomer = Convert.ToInt32(komnata.Attribute("номер").Value),
+                    vmestimost = Convert.ToInt32(komnata.Element("вместимость").Value),
+                    zaselenies = zaselenies.ToList()
+                });
+            }
+
+            foreach (var client in doc.Element("клиенты").Elements("клиент"))
+            {
+                root.clients.Add(new Client()
+                {
+                    cod = Convert.ToInt32(client.Element("код_кл").Value),
+                    fio = client.Element("фио").Value,
+                    pol = client.Element("пол").Value[0]
+                });
+            }
+
+            dataGridView1.DataSource = root.komnatas.Select(n => new { номер = n.nomer, вместимость = n.vmestimost }).ToArray();
+            dataGridView1.SelectionChanged += DataGridView1_SelectionChanged;
+            DataGridView1_SelectionChanged(new object(), EventArgs.Empty);
+
+            comboBox1.DataSource = root.komnatas.Select(n => n.nomer).ToArray();
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            ComboBox1_SelectedIndexChanged(new object(), EventArgs.Empty);
+
+            comboBox2.DataSource = root.komnatas.Select(n => n.nomer).ToArray();
+            comboBox2.SelectedIndexChanged += ComboBox2_SelectedIndexChanged;
+            ComboBox2_SelectedIndexChanged(new object(), EventArgs.Empty);
+
+            comboBox3.DataSource = root.clients.Select(n => n.fio).ToArray();
+            comboBox3.SelectedIndexChanged += ComboBox3_SelectedIndexChanged;
+            ComboBox3_SelectedIndexChanged(new object(), EventArgs.Empty);
+        }
+
         private void сгенерироватьНовыйXmlToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            generate_xml();
         }
 
         private void загрузитьСуществующийToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Xml файлы (*.xml)|*.xml";
+            if(fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.SelectionChanged -= DataGridView1_SelectionChanged;
+                comboBox1.SelectedIndexChanged -= ComboBox1_SelectedIndexChanged;
+                comboBox2.SelectedIndexChanged -= ComboBox2_SelectedIndexChanged;
+                comboBox3.SelectedIndexChanged -= ComboBox3_SelectedIndexChanged;
+            }
         }
     }
 }
